fe36254e7a920b61e8074001bee2ba63853bbac2
ABATarik <abaspahic.tarik@outlook.com>
2021-03-19 00:36:32 +0100

    nested array

---
+++ ex05/src/index.js
@@ -0,0 +1,12 @@
+var myNestedArray = [["Toblerone", 5], ["Milka", 3]];
+
+function myNestedFunction(arr) {
+    arr = myNestedArray;
+    return arr
+
+
+};
+
+console.log(myNestedFunction());
+
+module.exports = myNestedFunction;
